53eed5a8fedace053d277def69e14b9a2da040d1
CJ Barnes <cristopher.barnes@gmail.com>
2022-01-03 14:08:21 -0600

    delete child button added

--- src/components/UserChild/UserChild.jsx
+++ src/components/UserChild/UserChild.jsx
@@ -6,6 +6,7 @@ import { useStyles } from '../UserChild/UserChildStyles';
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import "./UserChild.css";
+import Button from "react-bootstrap/Button";
 
 
 function UserChild(props) {
@@ -13,10 +14,19 @@ function UserChild(props) {
   const history = useHistory();
   const dispatch = useDispatch();
   const classes = useStyles();
+  const user = useSelector((store) => store.user);
 
   useEffect(()=>{ 
     dispatch({type: 'UNSET_FORM'});
   }, []);
+
+  // let birthDate = props.child.birth_date;
+  // console.log(birthDate.toString().replace(/-/g, '.').split('T')[0]);
+  // console.log(birthDate.toLocaleString("en-US").replace(/-/g, '.').split('T')[0]);
+
+  const deleteChild = (childID) => {
+    console.log(childID);
+  }
 
   const getForm = (childID) => {
     console.log(childID);
@@ -34,7 +44,7 @@ function UserChild(props) {
       <Card
         className="childCard"
         onClick={() => getForm(props.child.id)}
-        style={{ width: "10rem" }}
+        style={{ width: "15rem" }}
       >
         <Card.Img
           alt={props.child.first_name}
@@ -43,28 +53,15 @@ function UserChild(props) {
         />
         <Card.Body>
           <Card.Title>{props.child.first_name} {props.child.last_name}</Card.Title>
+          <Card.Text className="mb-2">Allergies:<br />
+          {props.child.allergies}</Card.Text>
         </Card.Body>
       </Card>
+      {user.role === 'parent' ?
+        <Button variant="primary" style={{backgroundColor: 'lightgrey', color:'black', width: '15rem'}} className="btn" onClick={() => deleteChild(props.child.id)} > Delete </Button> :
+        <p></p>
+      }
     </Col>
-    // <div>
-    //   {/* <Box sx={{boxShadow: 3}} m={2} pt={1} >
-    //     <Grid item sx={3} > */}
-    //       <Card className={classes.root}>
-    //         <Typography gutterBottom variant="body1" component="div">{props.child.id} {props.child.first_name} {props.child.last_name}</Typography>
-    //         <CardMedia
-    //           component="img"
-    //           className={classes.media}
-    //           image={props.child.image_path}
-    //           id={props.child.id}
-    //           onClick={()=> handleClickEvent(props.child.id)}
-              
-    //         />
-    //         <Typography variant="body2" color="textSecondary">Allergies: {props.child.allergies}</Typography>
-    //         <br /><br />
-    //       </Card>
-    //     {/* </Grid>
-    //   </Box> */}
-    // </div>
   );
 }
 
