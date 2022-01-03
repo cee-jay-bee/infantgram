a6ee1c5135972143a3a07c0a24e59c9b323cf938
CJ Barnes <cristopher.barnes@gmail.com>
2022-01-03 13:06:58 -0600

    login page styled

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import {useSelector} from 'react-redux';
+import { Form, Button, Row, Col } from 'react-bootstrap';
 
 function LoginForm() {
   const [username, setUsername] = useState('');
@@ -25,41 +26,72 @@ function LoginForm() {
   }; // end login
 
   return (
-    <form className="formPanel" onSubmit={login}>
-      <h2>Login</h2>
+    <Form onSubmit={login} className='formPanel' style={{backgroundColor: 'lightgrey'}}>
+      <center><Form.Label style={{color: 'black'}}>Log In</Form.Label></center>
       {errors.loginMessage && (
         <h3 className="alert" role="alert">
           {errors.loginMessage}
         </h3>
       )}
-      <div>
-        <label htmlFor="username">
-          Username:
-          <input
-            type="text"
-            name="username"
-            required
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="password">
-          Password:
-          <input
-            type="password"
-            name="password"
-            required
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <input style={{backgroundColor: '#946E83'}} className="btn" type="submit" name="submit" value="Log In" />
-      </div>
-    </form>
+      
+      <Row >
+        <Col id="parentInfo">
+          <Form.Group className="mb-3" controlId="username">
+          <Form.Text>Username</Form.Text>
+          <Form.Control type="text" value={username}  onChange={(event) => setUsername(event.target.value)} required />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Form.Group className="mb-3" controlId="password">
+          <Form.Text>Password</Form.Text>
+          <Form.Control type="text" value={password} onChange={(event) => setPassword(event.target.value)} required />
+          </Form.Group>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+        <center><Button variant="primary" type="submit" style={{backgroundColor: '#946E83'}} className="btn" name="submit" > Log In </Button></center>
+        </Col>
+      </Row>
+    </Form>
+    
+    // <form className="formPanel" onSubmit={login}>
+    //   <h2>Login</h2>
+    //   {errors.loginMessage && (
+    //     <h3 className="alert" role="alert">
+    //       {errors.loginMessage}
+    //     </h3>
+    //   )}
+    //   <div>
+    //     <label htmlFor="username">
+    //       Username:
+    //       <input
+    //         type="text"
+    //         name="username"
+    //         required
+    //         value={username}
+    //         onChange={(event) => setUsername(event.target.value)}
+    //       />
+    //     </label>
+    //   </div>
+    //   <div>
+    //     <label htmlFor="password">
+    //       Password:
+    //       <input
+    //         type="password"
+    //         name="password"
+    //         required
+    //         value={password}
+    //         onChange={(event) => setPassword(event.target.value)}
+    //       />
+    //     </label>
+    //   </div>
+    //   <div>
+    //     <input style={{backgroundColor: '#946E83'}} className="btn" type="submit" name="submit" value="Log In" />
+    //   </div>
+    // </form>
   );
 }
 
--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -14,11 +14,12 @@ function RegisterPage() {
         <button
           type="button"
           className="btn btn_asLink"
+          style= {{color: "#f2f2f2"}}
           onClick={() => {
             history.push('/login');
           }}
         >
-          Login
+          already a member?
         </button>
       </center>
     </div>
